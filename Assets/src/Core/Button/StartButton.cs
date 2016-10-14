933b134369c98a6f0bb913240e60f90e086755b8
AREZ1718 <arez1718@gmail.com>
2016-10-15 02:37:14 +0300

    ит воркс

---
+++ Assets/src/Core/Button/StartButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+	 
+	public string playGame;
+
+	public void StartGame()
+	{
+		Application.LoadLevel(playGame);
+	}
+}
